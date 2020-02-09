8e73ecf5920253f1e35559c4f8640cc1c63fd848
github username <alekvuka@gmail.com>
2020-02-08 19:42:42 -0500

    create BackgroundAnimation class and file

---
+++ src/components/BackgroundAnimations.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class BackgroundAnimations extends Component {
+  render() {
+    return (
+      <div>'
+        background
+      </div>
+    )
+  }
+}
+
+export default BackgroundAnimations
