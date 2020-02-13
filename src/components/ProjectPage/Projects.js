0fa6b01848491cd6f82642f16c6f4cf66f23ef11
github username <alekvuka@gmail.com>
2020-02-13 00:11:20 -0500

    add react-touter package

--- src/components/ProjectPage/Projects.js
+++ src/components/ProjectPage/Projects.js
@@ -8,6 +8,7 @@ import {RemoveScroll} from 'react-remove-scroll';
 import Button from 'react-bootstrap/Button'
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 
 class Projects extends React.Component {
 
@@ -36,21 +37,27 @@ class Projects extends React.Component {
   render () {
   return (
     <div>
-    <Card border="primary" style={{ width: '18rem' }}>
 
-       <Card.Body>
-         <Card.Title>Primary Card Title</Card.Title>
-         <Card.Text>
-           Some quick example text to build on the card title and make up the bulk
-           of the card's content.
-         </Card.Text>
-             <Button variant="outline-primary" >GitHub</Button>
-             {"   "}
-             <Button variant="outline-danger" >WebSite</Button>
-
-       </Card.Body>
-     </Card>
-     <br />
+    <CSSTransitionGroup
+      transitionName="example"
+      transitionAppear={true}
+      transitionAppearTimeout={1000}
+      transitionEnter={false}
+      transitionLeave={false}>
+      <Card border="primary" style={{ width: '18rem' }}>
+         <Card.Body>
+           <Card.Title>Primary Card Title</Card.Title>
+           <Card.Text>
+             Some quick example text to build on the card title and make up the bulk
+             of the card's content.
+           </Card.Text>
+               <Button variant="outline-primary" >GitHub</Button>
+               {"   "}
+               <Button variant="outline-danger" >WebSite</Button>
+         </Card.Body>
+       </Card>
+       <br />
+    </CSSTransitionGroup>
     </div>
   );}
 }
