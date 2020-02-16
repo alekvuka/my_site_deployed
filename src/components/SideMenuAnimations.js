d332b0adb459a6bcd8da14c5f37bfa9cf176ede7
github username <alekvuka@gmail.com>
2020-02-16 16:21:08 -0500

    change blog navlink from blog page to link to medium

--- src/components/SideMenuAnimations.js
+++ src/components/SideMenuAnimations.js
@@ -60,10 +60,9 @@ class SideMenuAnimations extends Component {
           <Menu disableAutoFocus right pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" } itemListClassName={ "my-items" } burgerBarClassName={ this.state.color === 'red' ? "background2" : "background1"}>
             <NavLink exact to="/" activeStyle={{ background: 'pink'}}><h1>Home</h1></NavLink>
             <NavLink exact to="/projects" activeStyle={{ background: 'pink'}}><h1>Projects</h1></NavLink>
-            <NavLink exact to="/blog" activeStyle={{ background: 'pink'}}><h1>Blog</h1></NavLink>
+            <a href="https://medium.com/@aleksandarvuk"><h1>Blog</h1></a>
             <a href="https://www.linkedin.com/in/alekvuka/"><h1>LinkedIn</h1></a>
             <a href="https://github.com/alekvuka"><h1>GitHub</h1></a>
-            <a href=""><h1>Email</h1></a>
             <a href=""><h1>Contact</h1></a>
           </Menu>
         </CSSTransitionGroup>
